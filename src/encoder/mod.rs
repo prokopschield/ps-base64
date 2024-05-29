fee1f37cfdd25d6101d30ddbaadee04dc9b33009
Prokop <prokop.schield@protonmail.com>
2024-05-29 23:59:01 +0200

    feat(src/encoder): implement sized_encode()

--- src/encoder/mod.rs
+++ src/encoder/mod.rs
@@ -1,5 +1,44 @@
 pub const ALPHABET: &[u8; 64] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~_";
 
+#[inline(always)]
+pub fn sized_encode<const S: usize>(input: &[u8]) -> [u8; S] {
+    let mut output: [u8; S] = unsafe { std::mem::MaybeUninit::uninit().assume_init() };
+    let mut index = 0;
+
+    let mut push = |byte: u8| {
+        if index < S {
+            output[index] = byte;
+            index += 1;
+        }
+    };
+
+    for chunk in input.chunks_exact(3) {
+        push(ALPHABET[(chunk[0] >> 2) as usize]);
+        push(ALPHABET[(((chunk[0] & 0x3) << 4) | (chunk[1] >> 4)) as usize]);
+        push(ALPHABET[(((chunk[1] & 0xf) << 2) | (chunk[2] >> 6)) as usize]);
+        push(ALPHABET[(chunk[2] & 0x3f) as usize]);
+    }
+
+    let remainder = input.len() % 3;
+
+    if remainder == 1 {
+        let index: usize = input.len() - 1;
+
+        push(ALPHABET[(input[index] >> 2) as usize]);
+        push(ALPHABET[((input[index] & 3) << 4) as usize]);
+    } else if remainder == 2 {
+        let index: usize = input.len() - 2;
+
+        push(ALPHABET[(input[index] >> 2) as usize]);
+        push(ALPHABET[(((input[index] & 0x3) << 4) | (input[index + 1] >> 4)) as usize]);
+        push(ALPHABET[((input[index + 1] & 0xf) << 2) as usize]);
+    }
+
+    output[index..S].fill(b'=');
+
+    return output;
+}
+
 pub fn encode(input: &[u8]) -> String {
     let mut output = Vec::with_capacity(input.len() * 4 / 3 + 1);
 
--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,5 @@ pub use encoder::*;
 pub mod base64 {
     pub use crate::decoder::decode;
     pub use crate::encoder::encode;
+    pub use crate::encoder::sized_encode;
 }

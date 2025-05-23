67d64e6e9bd6faa06be839ec56e744f0e51d2e87
Prokop <prokop.schield@protonmail.com>
2025-05-23 23:47:28 +0200

    refactor: use match statement in char decoder

--- src/decoder/map.rs
+++ src/decoder/map.rs
@@ -1,19 +1,13 @@
 #[must_use]
 pub const fn decode_base64_char(char: u8) -> u8 {
-    if char >= b'0' && char <= b'9' {
-        char - b'0' + 52
-    } else if char >= b'A' && char <= b'Z' {
-        char - b'A'
-    } else if char >= b'a' && char <= b'z' {
-        char - b'a' + 26
-    } else if matches!(char, b'+' | b'-' | b'~') {
-        62
-    } else if matches!(char, b',' | b'/' | b'_') {
-        63
-    } else if matches!(char, b'=') {
-        0
-    } else {
-        char % 64
+    match char {
+        b'0'..=b'9' => char - b'0' + 52,
+        b'A'..=b'Z' => char - b'A',
+        b'a'..=b'z' => char - b'a' + 26,
+        b'+' | b'-' | b'~' => 62,
+        b',' | b'/' | b'_' => 63,
+        b'=' => 0,
+        char => char % 64,
     }
 }
 

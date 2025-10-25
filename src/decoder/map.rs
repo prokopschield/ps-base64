2e91970f064b5392c3e91471343d56b327ccb8ab
Prokop <prokop.schield@protonmail.com>
2025-10-26 00:05:05 +0200

    refactor: accept '.' as 62

--- src/decoder/map.rs
+++ src/decoder/map.rs
@@ -4,7 +4,7 @@ pub const fn decode_base64_char(char: u8) -> u8 {
         b'0'..=b'9' => char - b'0' + 52,
         b'A'..=b'Z' => char - b'A',
         b'a'..=b'z' => char - b'a' + 26,
-        b'+' | b'-' | b'~' => 62,
+        b'.' | b'+' | b'-' | b'~' => 62,
         b',' | b'/' | b'_' => 63,
         b'=' => 0,
         char => char % 64,
